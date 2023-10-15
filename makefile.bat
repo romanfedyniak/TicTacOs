466e0163b0438e9405843c844f7d814086d865d1
romanfedyniak <fedynuak.roma@gmail.com>
2023-10-15 20:00:34 +0300

    split the code

--- makefile.bat
+++ makefile.bat
@@ -1,4 +1,8 @@
 fasm src/tic_tac_toe.fasm bin/boot.bin
 if %errorlevel% neq 0 exit /b %errorlevel%
-qemu-img dd if=bin/boot.bin of=bin/disk.img bs=1440k
+fasm src/draw.fasm bin/draw.bin
+if %errorlevel% neq 0 exit /b %errorlevel%
+fasm src/image.fasm bin/disk.img
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 qemu-system-x86_64 -fda bin/disk.img -boot a -s
