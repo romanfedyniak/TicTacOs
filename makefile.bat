a68d2906de5244c2e87f7a73a650b376b0f982f4
romanfedyniak <fedynuak.roma@gmail.com>
2023-10-15 20:29:04 +0300

    using only one source code file

--- makefile.bat
+++ makefile.bat
@@ -1,8 +1,4 @@
-fasm src/tic_tac_toe.fasm bin/boot.bin
-if %errorlevel% neq 0 exit /b %errorlevel%
-fasm src/draw.fasm bin/draw.bin
-if %errorlevel% neq 0 exit /b %errorlevel%
-fasm src/image.fasm bin/disk.img
+fasm src/tic_tac_toe.fasm bin/disk.img
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 qemu-system-x86_64 -fda bin/disk.img -boot a -s
